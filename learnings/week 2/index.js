176b08e1a8d0168e665bec0b1d15cbc3e8187af9
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-11-07 19:54:07 +0530

    new learning of backend systems

--- learnings/week 2/index.js
+++ learnings/week 2/index.js
@@ -3,7 +3,7 @@ steps
 -> npm init -y
 -> make index.js
 -> npm install express
--> go on expressjs site and bring code 
+-> go on expressjs site and bring basic boiler code 
 */
 
 
@@ -11,13 +11,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/handleSum', (req, res) => {
-    let counter = req.query.counter;
+function handleFirstRequest(req, res){
+    // let counter = req.query.counter; // this is via query parameters
+
+    console.log(req.headers);
+    // in post man header section we clear url till handeSum and key -> counter and value -> 4
+    let counter = req.headers.counter; // this is via headers
+
     let calculatedSum = calculateSum(counter);
-    
     var ans = "The sum is " + calculatedSum;
     res.send(ans);
-})
+}
+
+// app.get('/handleSum', handleFirstRequest);
+app.post('/handleSum', handleFirstRequest); // this we check in postman 
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
