0a661bfef40110a5396f03d81674c34610599793
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-11-07 22:45:25 +0530

    learned middlewares and sending to backend via body

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MERN-stack-learning
-this is Nilay Patel
+
+Here i will keep on updating all the new things that i will be learning 
+<br>
+Also added comments for better understanding
--- learnings/week 2/index.js
+++ learnings/week 2/index.js
@@ -4,26 +4,47 @@ steps
 -> make index.js
 -> npm install express
 -> go on expressjs site and bring basic boiler code 
+-> npm install body-parser
 */
 
 
 const express = require('express')
+// added this after (npm install body-parser) and add (app.use(bodyParser.json());)
+let bodyParser = require('body-parser') 
 const app = express()
 const port = 3000
+
+
+
+
+// function middleWare1(req, res, next){ // request, response, next step
+//     console.log("from inside middleware " + req.headers.counter);
+//     // res.send("Error in middleWare"); // cannot send res if next is active, will have to put some condition
+//     next();
+// }
+
+// app.use(middleWare1); // this initializes our use of middleWare before the main function is called
+
+app.use(bodyParser.json()); // added a mew middleWare to extract the body when we do console.log(req.body)
+
 
 function handleFirstRequest(req, res){
     // let counter = req.query.counter; // this is via query parameters
 
-    console.log(req.headers);
-    // in post man header section we clear url till handeSum and key -> counter and value -> 4
-    let counter = req.headers.counter; // this is via headers
+    // console.log(req.headers);
+    // // in post man header section we clear url till handeSum and key -> counter and value -> 4
+    // let counter = req.headers.counter; // this is via headers
+
+    // console.log(req.body);
+    // for this in body in postman click on raw -> JSON
+    let counter = req.body.counter; // this is via body which we will use most of the time
 
     let calculatedSum = calculateSum(counter);
     var ans = "The sum is " + calculatedSum;
     res.send(ans);
 }
 
-// app.get('/handleSum', handleFirstRequest);
+// app.get('/handleSum', handleFirstRequest); // this can be accessed by the browser
 app.post('/handleSum', handleFirstRequest); // this we check in postman 
 
 
