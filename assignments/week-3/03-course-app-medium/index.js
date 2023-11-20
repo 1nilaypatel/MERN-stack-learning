e3583ba62a6b88d83b763f55be20c6b37509dfc2
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-11-20 22:39:49 +0530

    new learning

--- assignments/week-3/03-course-app-medium/index.js
+++ assignments/week-3/03-course-app-medium/index.js
@@ -1,52 +1,181 @@
+// /* 
+// steps 
+// -> npm init -y
+// -> make index.js
+// -> npm install express
+// -> go on expressjs site and bring basic boiler code 
+// -> npm install body-parser
+// -> npm install jsonwebtoken
+// */
+
 const express = require('express');
+const jwt = require('jsonwebtoken');
+let bodyParser = require('body-parser')
 const app = express();
+const port = 3000;
+
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+
+// reading files synchronously one by one using the try-catch block
+try{
+  ADMINS = JSON.parse(fs.readFileSync('admins.json', 'utf8'));
+  USERS = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+  COURSES = JSON.parse(fs.readFileSync('courses.json', 'utf8'));
+}catch{
+  ADMINS = [];
+  USERS = [];
+  COURSES = [];
+}
+
+const secretKey = n1laypat2l;
+const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if(authHeader){
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, secretKey, (err, user) => {
+      if(err){
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  }else{
+    res.sendStatus(401);
+  }
+}
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+
+  const {username, password} = req.body;
+  const admin = ADMINS.find(a => a.username === username);
+  if(admin){
+    res.status(403).json({ message: 'Admin already exists' });
+  }else{
+    const newAdmin = {username, password};
+    ADMINS.push(newAdmin);
+    fs.writeFileSync('admins.json', JSON.stringify(ADMINS)); // writing using node.js file system SYNCHRONOUS model 
+    const token = jwt.sign({username, role: 'admin'}, secretKey, {expiresIn: '1h'}); // generating JWT and also specifying roles properties
+    res.json({ message: 'Admin created successfully', token });
+  }
 });
 
 app.post('/admin/login', (req, res) => {
   // logic to log in admin
+
+  const { username, password } = req.headers;
+  const admin = ADMINS.find(a => a.username === username && a.password === password);
+  if (admin) {
+    const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Logged in successfully', token });
+  } else {
+    res.status(403).json({ message: 'Invalid username or password' });
+  }
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses', authenticateJwt, (req, res) => {
   // logic to create a course
+
+  const course = req.body;
+  course.id = COURSES.length + 1;
+  COURSES.push(course);
+  fs.writeFileSync('courses.json', JSON.stringify(COURSES));
+  res.json({ message: 'Course created successfully', courseId: course.id });
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put('/admin/courses/:courseId', authenticateJwt, (req, res) => {
   // logic to edit a course
+
+  const course = COURSES.find(c => c.id === parseInt(req.params.courseId));
+  if (course) {
+    Object.assign(course, req.body);
+    fs.writeFileSync('courses.json', JSON.stringify(COURSES));
+    res.json({ message: 'Course updated successfully' });
+  } else {
+    res.status(404).json({ message: 'Course not found' });
+  }
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get('/admin/courses', authenticateJwt, (req, res) => {
   // logic to get all courses
+
+  res.json({ courses: COURSES });
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
+
+  const { username, password } = req.body;
+  const user = USERS.find(u => u.username === username);
+  if (user) {
+    res.status(403).json({ message: 'User already exists' });
+  } else {
+    const newUser = { username, password };
+    USERS.push(newUser);
+    fs.writeFileSync('users.json', JSON.stringify(USERS));
+    const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
+    res.json({ message: 'User created successfully', token });
+  }
 });
 
 app.post('/users/login', (req, res) => {
   // logic to log in user
+
+  const {username, password} = req.headers;
+  const user = USERS.find(u => u.username === username && u.password === password);
+  if (user) {
+    const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Logged in successfully', token });
+  } else {
+    res.status(403).json({ message: 'Invalid username or password' });
+  }
 });
 
-app.get('/users/courses', (req, res) => {
+app.get('/users/courses', authenticateJwt, (req, res) => {
   // logic to list all courses
+
+  res.json({ courses: COURSES });
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post('/users/courses/:courseId', authenticateJwt, (req, res) => {
   // logic to purchase a course
+
+  const course = COURSES.find(c => c.id === parseInt(req.params.courseId));
+  if (course) {
+    const user = USERS.find(u => u.username === req.user.username);
+    if (user) {
+      if (!user.purchasedCourses) {
+        user.purchasedCourses = [];
+      }
+      user.purchasedCourses.push(course);
+      fs.writeFileSync('users.json', JSON.stringify(USERS));
+      res.json({ message: 'Course purchased successfully' });
+    } else {
+      res.status(403).json({ message: 'User not found' });
+    }
+  } else {
+    res.status(404).json({ message: 'Course not found' });
+  }
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get('/users/purchasedCourses', authenticateJwt, (req, res) => {
   // logic to view purchased courses
+
+  const user = USERS.find(u => u.username === req.user.username);
+  if(user){
+    res.json({purchasedCourses: user.purchasedCourses || []});
+  }else{
+    res.status(403).json({ message: 'User not found' });
+  }
 });
 
 app.listen(3000, () => {
--- assignments/week-3/solutions/04-course-app-hard.js
+++ assignments/week-3/solutions/04-course-app-hard.js
@@ -1,3 +1,13 @@
+// /* 
+// steps 
+// -> npm init -y
+// -> make index.js
+// -> npm install express
+// -> go on expressjs site and bring basic boiler code 
+// -> npm install body-parser
+// -> npm install jsonwebtoken
+// */
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
@@ -8,9 +18,21 @@ app.use(express.json());
 const SECRET = 'SECr3t';  // This should be in an environment variable in a real application
 
 // Define mongoose schemas
+// schema is a blueprint for defining the structure of a document within a MongoDB collection. 
 const userSchema = new mongoose.Schema({
-  username: {type: String},
+  // username: {type: String},
+  username: String,
   password: String,
+
+  // The square brackets [] indicate that purchasedCourses is an array.
+
+  // type: mongoose.Schema.Types.ObjectId
+// Specifies that each element in the array is expected to be an ObjectId. ObjectId is a special type used in MongoDB for unique identifiers.
+
+// ref: 'Course'
+// Indicates that the ObjectId references documents in another collection named 'Course'. This establishes a relationship or reference between the purchasedCourses array and the 'Course' collection.
+
+// an array of purchasedCourses that contains references to 'Course' documents. This schema allows for establishing relationships between the 'User' and 'Course' collections in MongoDB. The ref property helps Mongoose understand that the purchasedCourses field should contain references to documents in the 'Course' collection.
   purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
 });
 
@@ -28,6 +50,7 @@ const courseSchema = new mongoose.Schema({
 });
 
 // Define mongoose models
+// After defining schemas, Mongoose models are created using mongoose.model. Models represent collections in MongoDB and provide an interface for interacting with the data.
 const User = mongoose.model('User', userSchema);
 const Admin = mongoose.model('Admin', adminSchema);
 const Course = mongoose.model('Course', courseSchema);
@@ -38,6 +61,7 @@ const authenticateJwt = (req, res, next) => {
     const token = authHeader.split(' ')[1];
     jwt.verify(token, SECRET, (err, user) => {
       if (err) {
+        // return is must to terminate and so to next process
         return res.sendStatus(403);
       }
       req.user = user;
@@ -49,25 +73,44 @@ const authenticateJwt = (req, res, next) => {
 };
 
 // Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
+// The code connects to a MongoDB database using mongoose.connect. The connection URI specifies the MongoDB server address, credentials, and the name of the database (courses in this case).
+// Options such as useNewUrlParser and useUnifiedTopology are provided to ensure compatibility with the latest MongoDB driver.
+// Database Name:
+
+// The dbName option in mongoose.connect specifies the name of the database within the MongoDB server that Mongoose will use. In this case, it's set to "courses."
 mongoose.connect('mongodb+srv://kirattechnologies:iRbi4XRDdM7JMMkl@cluster0.e95bnsi.mongodb.net/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
 
-app.post('/admin/signup', (req, res) => {
+app.post('/admin/signup', async (req, res) => {
   const { username, password } = req.body;
-  function callback(admin) {
-    if (admin) {
-      res.status(403).json({ message: 'Admin already exists' });
-    } else {
-      const obj = { username: username, password: password };
-      const newAdmin = new Admin(obj);
-      newAdmin.save();
-      const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
-      res.json({ message: 'Admin created successfully', token });
-    }
-
+  // findOne: This is a Mongoose method used to find a SINGLE document in the collection that matches the specified conditions.
+  const admin = await Admin.findOne({username});
+  if(admin){
+    res.status(403).json({message: "Admin already exists"})
+  }else{
+    const obj = { username: username, password: password };
+    const newAdmin = new Admin(obj);
+    await newAdmin.save();
+    const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Admin created successfully', token });
   }
-  Admin.findOne({ username }).then(callback);
 });
+
+// app.post('/admin/signup', (req, res) => {
+//   const { username, password } = req.body;
+//   function callback(admin) {
+//     if (admin) {
+//       res.status(403).json({ message: 'Admin already exists' });
+//     } else {
+//       const obj = { username: username, password: password };
+//       const newAdmin = new Admin(obj);
+//       newAdmin.save();
+//       const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+//       res.json({ message: 'Admin created successfully', token });
+//     }
+
+//   }
+//   Admin.findOne({ username }).then(callback);
+// });
 
 app.post('/admin/login', async (req, res) => {
   const { username, password } = req.headers;
@@ -87,6 +130,7 @@ app.post('/admin/courses', authenticateJwt, async (req, res) => {
 });
 
 app.put('/admin/courses/:courseId', authenticateJwt, async (req, res) => {
+  // { new: true }: This option ensures that the method returns the modified document rather than the original. If not specified or set to false, it would return the original document before the update.
   const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, { new: true });
   if (course) {
     res.json({ message: 'Course updated successfully' });
@@ -96,6 +140,7 @@ app.put('/admin/courses/:courseId', authenticateJwt, async (req, res) => {
 });
 
 app.get('/admin/courses', authenticateJwt, async (req, res) => {
+  // find: This is a Mongoose method used to retrieve documents from the collection that match the specified conditions. When no conditions are provided (an empty object {}), it fetches all documents in the collection.
   const courses = await Course.find({});
   res.json({ courses });
 });
@@ -148,6 +193,9 @@ app.post('/users/courses/:courseId', authenticateJwt, async (req, res) => {
 });
 
 app.get('/users/purchasedCourses', authenticateJwt, async (req, res) => {
+  // populate: This is a Mongoose method used to replace specified paths in the document with document(s) from other collection(s). It's a way to "populate" the document with referenced documents from another collection.
+
+// 'purchasedCourses': This is the path to the field in the user document that needs to be populated. In this case, it's the 'purchasedCourses' field, which likely contains references (ObjectIds) to courses in the "Course" collection.
   const user = await User.findOne({ username: req.user.username }).populate('purchasedCourses');
   if (user) {
     res.json({ purchasedCourses: user.purchasedCourses || [] });
