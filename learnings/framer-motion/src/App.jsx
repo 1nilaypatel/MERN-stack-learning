58f1cb6728f6eedf28c5939e2787b299d001fb24
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-12 01:08:05 +0530

    started Framer-Motion

---
+++ learnings/framer-motion/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AnimationControls from './components/AnimationControls'
+import BasicsOfMotion from './components/BasicsOfMotion'
+import Gestures from './components/Gestures'
+import ScrollAnimations from './components/ScrollAnimation'
+import ViewBasedAnimations from './components/ViewBaseAnimaions'
+
+export default function App() {
+  return (
+    <div>
+      <BasicsOfMotion />
+      {/* <Gestures /> */}
+      {/* <AnimationControls /> */}
+      {/* <ViewBasedAnimations /> */}
+      {/* <ScrollAnimations /> */}
+    </div>
+  )
+}
